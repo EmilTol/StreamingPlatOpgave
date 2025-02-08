34a1e2194e3eea9725be9ab28b909be7346fe7dd
EmilTol <erazertt@gmail.com>
2025-02-09 00:15:28 +0100

    Håber det virker Tilføjet UseCase og Favorite

--- src/main/java/com/example/streamingplatopgave/Favorite.java
+++ src/main/java/com/example/streamingplatopgave/Favorite.java
@@ -1,4 +1,23 @@
 package com.example.streamingplatopgave;
 
 public class Favorite {
+
+    private int userId;
+    private int movieId;
+
+    // Constructor
+    public Favorite(int userId, int movieId) {
+        this.userId = userId;
+        this.movieId = movieId;
+    }
+
+    // Getter for userId
+    public int getUserId() {
+        return userId;
+    }
+
+    // Getter for movieId
+    public int getMovieId() {
+        return movieId;
+    }
 }
--- src/main/java/com/example/streamingplatopgave/UseCase.java
+++ src/main/java/com/example/streamingplatopgave/UseCase.java
@@ -1,11 +1,44 @@
 package com.example.streamingplatopgave;
 
+import java.util.List;
+
 public class UseCase {
 
-    public static List<String> getFavoritesByEmail (String email) {
+    private static final DatabaseHandler db = new DatabaseHandler();
 
+    public String createUser(String firstName, String lastName, String email, String subscriptionType){
+        User user = new User(0, firstName, lastName, email, subscriptionType);
+        return db.createUserObject(user);
+    }
+
+    public String updateUser(int userId, String firstName, String lastName, String email, String subscriptionType) {
+        User user = new User(userId, firstName, lastName, email, subscriptionType);
+        return db.updateUserObject(user);
+    }
+
+    public String deleteUser(String email) {
+//        if (email == null || email.isEmpty()) {
+//            return "Skal inkludere email";
+//        }
+        return db.deleteUserObject(email);
+    }
+
+    public List<User> getAllUsers() {
+
+        return db.getAllUsers();
     }
 
 
+    public List<Movie> getMoviesSortedByRating(){
+        return db.showMoviesByRating();
+    }
+
+    public List<Movie> getFavoriteMoviesByEmail(String email){
+        return db.getFavoriteMovies(email);
+    }
+
+    public String addFavoriteMovie(int userID, int movieID){
+        return db.addFavoriteMovie(userID, movieID);
+    }
 
 }
