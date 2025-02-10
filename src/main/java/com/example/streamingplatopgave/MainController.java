180571fbf241991ae5fd8e0240a86651d9f26768
ThomasKiszely <thomas@bohn-jacobsen.dk>
2025-02-10 21:57:48 +0100

    tilføjet updateknap til fsvoritter

--- src/main/java/com/example/streamingplatopgave/MainController.java
+++
@@ -1,4 +0,0 @@
-package com.example.streamingplatopgave;
-
-public class MainController {
-}
--- src/main/java/com/example/streamingplatopgave/UseCase.java
+++ src/main/java/com/example/streamingplatopgave/UseCase.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public class UseCase {
 
-    String email;
     User user;
     private static final DatabaseHandler db = new DatabaseHandler();
 
